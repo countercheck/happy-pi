b28bfe5b124a930a95c9d4c0be374327ee89d298
Michael Atlin <matlin@infotech.com>
2016-04-28 17:34:25 -0400

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+$(function() {
+  $("#happy").click(function(){
+    console.log('happy');
+    logEmotion('happy');
+  });
+
+  function logEmotion(emotion){
+    $.ajax({
+      url: 'https://sf9pegpjp4.execute-api.us-west-2.amazonaws.com/staging/emotions',
+      type: 'post',
+      data: {
+      },
+      headers: {
+      },
+      dataType: 'json',
+      success: function (data) {
+          console.info(data);
+      }
+    });
+  }
+}
